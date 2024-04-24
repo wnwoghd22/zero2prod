b59cfb1bca02dd762609b490bc2fabe3bbf93eb8
Jay <wnwoghd22@gmail.com>
2024-04-24 21:38:29 +0900

    random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("port: {}", port);
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,16 @@
 //! tests/health_check.rs
 
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn dummy_test() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("failed to execute request.");
@@ -18,9 +20,12 @@ async fn dummy_test() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    // todo!()
-    let server = zero2prod::run().expect("failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("failed to bind address");
 
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }

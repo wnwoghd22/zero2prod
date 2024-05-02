3dc1ace56b9129fa0d60763d0f95a38b779a5d66
Jay <wnwoghd22@gmail.com>
2024-05-02 19:27:59 +0900

    7.2.4

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -62,7 +62,11 @@ async fn spawn_app() -> TestApp {
         .email_client
         .sender()
         .expect("invalid sender email address");
-    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
 
     let server = zero2prod::startup::run(listener, connection_pool.clone(), email_client)
         .expect("failed to bind address");

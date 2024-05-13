c0d39654e5bcb776f512a9a72fb3ef8326442ff0
Jay <wnwoghd22@gmail.com>
2024-05-13 16:44:47 +0900

    10.2.3~(5)
    sqlx migration은 immutable하다. 신중하게 할 것.

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -6,6 +6,7 @@ use reqwest::{
     StatusCode,
 };
 use secrecy::{ExposeSecret, Secret};
+use sha3::Digest;
 use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
@@ -163,14 +164,17 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
+    let password_hash = format!("{:x}", password_hash);
+
     let user_id: Option<_> = sqlx::query!(
         r#"
         SELECT user_id
         FROM users
-        WHERE username = $1 AND password = $2
+        WHERE username = $1 AND password_hash = $2
         "#,
         credentials.username,
-        credentials.password.expose_secret()
+        password_hash
     )
     .fetch_optional(pool)
     .await

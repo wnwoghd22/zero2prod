f6c53bced2fb7daf6e8abcac9277282babbf170d
Jay <wnwoghd22@gmail.com>
2024-05-19 13:18:58 +0900

    10.6

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,13 @@
 use super::routes::*;
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
+use actix_web::cookie::Key;
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
-use secrecy::Secret;
+use actix_web_flash_messages::storage::CookieMessageStore;
+use actix_web_flash_messages::FlashMessagesFramework;
+use secrecy::{ExposeSecret, Secret};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -79,9 +82,13 @@ pub fn run(
     let db_pool = web::Data::new(db_pool); // Arc
     let email_client = web::Data::new(email_client);
     let base_url = web::Data::new(ApplicationBaseUrl(base_url));
+    let message_store =
+        CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(message_framework.clone())
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(home))
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -15,9 +15,6 @@ async fn an_error_flash_meassage_is_set_on_failure() {
     // Assert
     assert_is_redirect_to(&response, "/login");
 
-    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
-    assert_eq!(flash_cookie.value(), "Authentication failed");
-
     // Act 1
     let html_page = app.get_login_html().await;
     assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
